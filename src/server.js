82c896d8612a8d1306ea686a4e5daf3874cf0a31
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-05-07 23:29:44 +0200

    Online or offline this is the question

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@
  const app = express('./app');
  const server = require('http').createServer(app)
  const chalk = require('chalk');
- const userController=require('./controllers/user')
+ const userController=require('./controllers/user');
+const user = require('./models/user');
  const io = require('socket.io')(server,{
     cors:{
         origins: ['http://localhost:4200/**','http://localhost:5000/**']
@@ -21,7 +22,7 @@
   server.listen(5000, function () {
     console.log(`\n>> ${chalk.bold.magenta("WebSockets are listening at port:")}  ${chalk.bold.yellow(5000)}\n`);
 });
-
+let usersConnected=[];
  io.on('connection', function (socket) {
     console.log("hola");
     
@@ -37,26 +38,34 @@
          console.log(`${chalk.red(`Sin payload`)}`);
      
      } else {
-         console.log(payload);
-        const userId = JSON.parse(payload).id 
         console.log(payload);
-        console.log(chalk.bold.magenta("New device connected: "+ userId +" >> " )+ chalk.bold.blue(JSON.parse(payload).email));
-         /**
-          * Una vez enviado la informacion del usuario conectado en este caso es un peequeño objecto que contiene nombre y id,
-          * creamos una sala y lo unimos https://socket.io/docs/rooms-and-namespaces/
-          */
-         console.log(payload);
+        const userId = JSON.parse(payload).id 
+        const userEmail = JSON.parse(payload).email 
+        const userChats = JSON.parse(payload).chats;
+        console.log(payload);
+        console.log(chalk.bold.magenta("User connected: "+ userId +" >> " )+ chalk.bold.blue(userEmail));
     
- 
+
+         
+         if(userChats.length!=0){
+             for (let chat of userChats) {
+                socket.join(`chat_${chat}`);
+                console.log(`${chalk.yellow(`* ${userEmail} has joined to >> `)+`${`chat_${chat}`}`}`);
+             }
+         }
+         socket.broadcast.emit('userConnected',userId);
  
          /**
           * --------- EMITIR -------------
           * Para probar la conexion con el dispositivo unico le emitimos un mensaje a el dispositivo conectado
           */
-         socket.emit('message', {
-             msg: `Hola tu eres el dispositivo ${userId}, perteneces a la sala room_${payload.id}, de ${payload.user}`
+         
+        
+         socket.on('askWhoAreConnected',(name)=>{
+             usersConnected.push(name);
+             usersConnected=  [... new Set(usersConnected)];
+             socket.emit('anwserWhoAreConnected',usersConnected);
          });
- 
          /**
           * ----------- ESCUCHAR -------------
           * Cuando el cliente nos emite un mensaje la api los escucha de la siguiente manera
@@ -64,17 +73,11 @@
          socket.on('default', function(res){
  
              switch (res.event) {
-                 case 'message':
+                 case 'chatSelected':
                      /**
                       * Si el evento que escucha es "message", se parsea la informacion recibida
                       * y posteriormente se emite un "message" a todos los dispositivos unidos a la sala.
                       */
-                     const inPayloadCookie = JSON.parse(res.cookiePayload);
-                     const inPayload = res.payload;
- 
-                     io.to(`room_${inPayloadCookie.id}`).emit('message',{
-                         msg: `Mensaje a todos los dispositivos de la sala room__${inPayloadCookie.id}: ${inPayload.message}`
-                     });
  
                      break;
                  default:
@@ -90,6 +93,7 @@
       */
      socket.on('disconnect', function () {
          console.log('User disconnected >>',chalk.bold.blue(JSON.parse(payload).email));
+         socket.broadcast.emit('userDisconnected',JSON.parse(socket.handshake.query.payload).id);
      });
  });
 
