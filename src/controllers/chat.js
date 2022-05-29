78605b883a421a55641a2340b49f76b7337c0804
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-05-29 23:41:55 +0200

    Refactoring upload methods

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -66,142 +66,20 @@ async function putChat(req, res, next) {
 }
 
 async function uploadVideo(req, res, next) {
-  console.log("fd");
-  if (res.status(400)) {
-    console.log("ccvb");
-  }
-  const params = req.params;
-  console.log(params);
-  const user = await User.findById(params.idUser);
-  if(user){
-  Chat.findById(params.idChat, async (err, chatData) => {
-    if (err) {
-      console.log(err.toString());
-      next(err);
-    } else {
-      if (!chatData) {
-        res.status(404).send({ error: "❌ Cannot found chat!" });
-      } else {
-        let chat = chatData;
-        console.log(req.files);
-        if (req.files) {
-          console.log();
-          const filePath = req.files.video.path;
-          let fileSplit = filePath.split(path.delimiter);
-          if (fileSplit.length == 1) {
-            //Porque no me pilla la doble barra invertida
-            fileSplit = filePath.split("\\");
-          }
-          let fileName = fileSplit[0];
-          if (fileName == "uploads") {
-            fileName = fileSplit[1];
-          }
-          let extSplit = fileName.split(".");
-          let fileExt = extSplit[1];
-          console.log(fileName, fileExt, extSplit);
-            let newMessage = new Message();
-            newMessage.type = "video";
-            newMessage.url = fileName;
-            newMessage.chat = chat._id;
-            newMessage.time = `${('0'+(new Date().getHours())).slice(-2)}:${('0'+(new Date().getMinutes())).slice(-2)}`;
-            newMessage.author = params.idUser;
-
-            await newMessage.save();
-            chat.messages.push(newMessage._id);
-            console.log(chat);
-            const chatResult = await Chat.updateOne(
-              Chat.findById(chat._id),
-              chat
-            );
-            console.log(chatResult);
-            if (!chatResult) {
-              res.status(404).send({ error: "❌ Cannot found chat!" });
-            } else {
-              res.status(200).send({ msg: "✅ Video uploaded!" });
-            }
-        }
-      }
-    }
-  });}else{
-    res.status(400).send({error: "❌ Cannot found user!" })
-  }
+  upload(req,res,next,"video");
 }
 
 async function uploadAudio(req, res, next) {
-  console.log("fd");
-  if (res.status(400)) {
-    console.log("ccvb");
-  }
-  const params = req.params;
-  console.log(params);
-  const user = await User.findById(params.idUser);
-  if(user){
-  Chat.findById(params.idChat, async (err, chatData) => {
-    if (err) {
-      console.log(err.toString());
-      next(err);
-    } else {
-      if (!chatData) {
-        res.status(404).send({ error: "❌ Cannot found chat!" });
-      } else {
-        let chat = chatData;
-        console.log(req.files);
-        if (req.files) {
-          console.log();
-          const filePath = req.files.audio.path;
-          let fileSplit = filePath.split(path.delimiter);
-          if (fileSplit.length == 1) {
-            //Porque no me pilla la doble barra invertida
-            fileSplit = filePath.split("\\");
-          }
-          let fileName = fileSplit[0];
-          if (fileName == "uploads") {
-            fileName = fileSplit[1];
-          }
-          let extSplit = fileName.split(".");
-          let fileExt = extSplit[1];
-          console.log(fileName, fileExt, extSplit);
-          if (fileExt !== "webm" && fileExt !== "mp3") {
-            res.status(400).send({
-              error: "❌ Audio extension is not allowed (Only .webm or .mp3)",
-            });
-          } else {
-            let newMessage = new Message();
-            newMessage.type = "audio";
-            newMessage.url = fileName;
-            newMessage.chat = chat._id;
-            newMessage.time = `${('0'+(new Date().getHours())).slice(-2)}:${('0'+(new Date().getMinutes())).slice(-2)}`;
-            newMessage.author = params.idUser;
-
-            await newMessage.save();
-            chat.messages.push(newMessage._id);
-            console.log(chat);
-            const chatResult = await Chat.updateOne(
-              Chat.findById(chat._id),
-              chat
-            );
-            console.log(chatResult);
-            if (!chatResult) {
-              res.status(404).send({ error: "❌ Cannot found chat!" });
-            } else {
-              res.status(200).send({ msg: "✅ Audio uploaded!" });
-            }
-          }
-        }
-      }
-    }
-  });}else{
-    res.status(400).send({error: "❌ Cannot found user!" })
-  }
+  upload(req,res,next,"audio");
 }
 
 
-async function uploadImage(req, res, next) {
-  console.log("fd");
-  if (res.status(400)) {
-    console.log("ccvb");
-  }
-  const params = req.params;
+function uploadImage(req, res, next) {
+  upload(req,res,next,"image");
+}
+
+async function upload(req,res,next,typeMessage){
+    const params = req.params;
   const user = await User.findById(params.idUser);
   if(user){
   Chat.findById(params.idChat, async (err, chatData)  => {
@@ -215,8 +93,7 @@ async function uploadImage(req, res, next) {
         let chat = chatData;
         console.log(req.files);
         if (req.files) {
-          console.log();
-          const filePath = req.files.image.path;
+          const filePath = req.files.file.path;
           let fileSplit = filePath.split(path.delimiter);
           if (fileSplit.length == 1) {
             //Porque no me pilla la doble barra invertida
@@ -228,7 +105,7 @@ async function uploadImage(req, res, next) {
           }
 
           let newMessage = new Message();
-            newMessage.type = "image";
+            newMessage.type = typeMessage;
             newMessage.url = fileName;
             newMessage.chat = chat._id;
             newMessage.time = `${('0'+(new Date().getHours())).slice(-2)}:${('0'+(new Date().getMinutes())).slice(-2)}`;
@@ -239,13 +116,13 @@ async function uploadImage(req, res, next) {
             console.log(chat);
 
             const chatResult = await Chat.updateOne(
-              Chat.findById(chat._id),
+              await Chat.findById(chat._id),
               chat
             );
             if (!chatResult) {
               res.status(404).send({ error: "❌ Cannot found chat!" });
             } else {
-              res.status(200).send({ msg: "✅ Image uploaded!" });
+              res.status(200).send({ msg: `✅ ${typeMessage} uploaded!` });
             }
         }
       }
